7e2823b05cf594e8581f3054882d1ee794851f27
Dani Sándor <sandor.dani98@gmail.com>
2019-03-24 21:05:04 +0100

    Szekrény kész

--- src/tile/Cupboard.java
+++ src/tile/Cupboard.java
@@ -1,14 +1,48 @@
 package tile;
 
+import java.util.Random;
+
+import animal.Animal;
+import def.Logger;
+
 /**
  * Szekrényt reprezentáló osztály
  *
  */
 public class Cupboard extends Tile {
-///////////////
-	/// lehetne hogy a szomszédai a többi szekrény (nem a fizikai szomszédai azt nem kell tudnia)
-	// és annyi hogy oda bassza át õket az acceptban ahogy a diagramon
-//////////////kérdés ilyenkor a szomszédokat meg beállítást itt is jelezni kell vagy elég a csempében itt meg kommentben írjuk hogy máshogy használjuk
+	
+	Cupboard[] cupboards;
+	
+	public void SetCupboards(Cupboard[] c) {
+		Logger.get_static_logger().enter(this, "Accept", new Object[] {c});
+		
+		cupboards=c;
+		
+		Logger.get_static_logger().exit(this, "Accept", new Object[] {c},"");
+		
+	}
+	
+	public boolean Accept(Animal a) {
+		Logger.get_static_logger().enter(this, "Accept", new Object[] {a});
+		
+		Animal oldAnimal=myAnimal;
+		
+		if(!myAnimal.equals(null)) myAnimal.HitBy(a);
+		else {
+			Random rand=new Random();
+			a.Move(cupboards[rand.nextInt(cupboards.length)]);
+		}
+		//Orángután elkapja a pandát esetben fut le (csak ekkor változik az állat)
+		if(myAnimal!=oldAnimal) {
+			Random rand=new Random();
+			a.Move(cupboards[rand.nextInt(cupboards.length)]);
+		}
+		
+		
+		Logger.get_static_logger().exit(this, "Accept", new Object[] {a},"");
+		return true;
+	}
+	
 	public void Step() {
 		
 	}
