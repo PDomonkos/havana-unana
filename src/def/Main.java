e9e6ed19d794b6d56a5b979966c1ac3575029e75
tuskajozsef <47720206+tuskajozsef@users.noreply.github.com>
2019-03-25 01:25:30 +0100

    Bővítés és exception kezelés

--- src/def/Main.java
+++ src/def/Main.java
@@ -17,23 +17,6 @@ public class Main {
 	 * @param args parancssori argumentumok
 	 */
 	public static void main(String[] args) {
-		in = new Scanner(System.in);
-				
-		System.out.println("Havana Beach Skeleton\n");
-		System.out.println("1: Panda lép");
-		System.out.println("2: Pandát törékeny csempére húznak, miközben õ is húz egy pandát.");
-		System.out.println("3: Orángután törékeny csempére lép és lezuhan");
-		System.out.println("4: Éhes panda csilingelés hatására ugrik egyet");
-		System.out.println("5: Lusta panda leül");
-		System.out.println("6: Orángután megfog egy pandát, miközben húz egyet");
-		System.out.println("7: Orángután lép, miközben húz egy pandát");
-		System.out.println("8: Orángután áthúz egy Pandát egy Cupboardon keresztül");
-		System.out.println("9: Orángután kivisz egy Pandát a kijáraton");
-		System.out.println("10: Ijedõs Panda megijed és elengedi a mögötte álló kezét");
-		
-		System.out.print("Válassz menüpontot: ");
-		String input = in.nextLine();
-		
 		// Scenariok betoltese a map-be
 		HashMap<String, Scenario> Scenarios = new HashMap<String, Scenario>();
 		Scenarios.put("1", new PandaSteps_1());
@@ -51,8 +34,38 @@ public class Main {
 		Logger.init_static_logger();
 				
 		// Scenario kivalasztasa es futtatasa
+		
+		in = new Scanner(System.in);
+		String input="";
+		
+		while(!input.equals("0")) {
+		System.out.println("Havana Beach Skeleton\n");
+		System.out.println("1: Panda lép");
+		System.out.println("2: Pandát törékeny csempére húznak, miközben õ is húz egy pandát.");
+		System.out.println("3: Orángután törékeny csempére lép és lezuhan");
+		System.out.println("4: Éhes panda csilingelés hatására ugrik egyet");
+		System.out.println("5: Lusta panda leül");
+		System.out.println("6: Orángután megfog egy pandát, miközben húz egyet");
+		System.out.println("7: Orángután lép, miközben húz egy pandát");
+		System.out.println("8: Orángután áthúz egy Pandát egy Cupboardon keresztül");
+		System.out.println("9: Orángután kivisz egy Pandát a kijáraton");
+		System.out.println("10: Ijedõs Panda megijed és elengedi a mögötte álló kezét");
+		System.out.println("0: Kilépés");
+		
+		System.out.print("Válassz menüpontot: ");
+		input = in.nextLine();
+	
+		try {
+			if(input.equals("0"))
+				System.exit(0);
 		Scenario selected = Scenarios.get(input);
 		selected.execute();
+		}
+		
+		catch(Exception e) {
+			System.out.println("Nincs ilyen menüpont!");
+		}
+		}
 			
 		in.close();
 	}
