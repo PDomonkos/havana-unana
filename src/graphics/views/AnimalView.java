f9ba634fc43815b052b30ee951bba26466771f28
ricsi98 <98kissricsi@gmail.com>
2019-05-09 19:31:59 +0200

    OrangutanView AnimalView

--- src/graphics/views/AnimalView.java
+++ src/graphics/views/AnimalView.java
@@ -1,9 +1,17 @@
 package graphics.views;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import animal.Animal;
+import def.Coord;
+import def.Game;
 import graphics.Drawable;
+import tile.Tile;
 
 public class AnimalView extends Drawable {
 	
@@ -11,12 +19,28 @@ public class AnimalView extends Drawable {
 	
 	public AnimalView(Animal a) {
 		this.a = a;
+		try {
+			this.SetImage(ImageIO.read(new File("resources/panda_img.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void Draw(Graphics g) {
-		// TODO Auto-generated method stub
-
+		Tile t = a.getTile();
+		Coord c = Game.GetCoords(t);
+		c = Coord.Scale(c, Game.scale);
+		
+		if (a.GetFollower() != null) {
+			t = a.GetFollower().getTile();
+			Coord c2 = Game.GetCoords(t);
+			c2 = Coord.Scale(c, Game.scale);
+			g.setColor(Color.black);
+			g.drawLine((int)c2.GetX(), (int)c2.GetY(), (int)c.GetX(), (int)c.GetY());
+		}
+		
+		g.drawImage(img, (int)c.GetX(), (int)c.GetY(), 50, 50, null);
 	}
 
 }
