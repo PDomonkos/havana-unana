a0d742410c1d6ddbc89a64d933cfa506315d8515
Tuska <tuska.jozsef@outlook.com>
2019-05-09 18:59:34 +0200

    AnimalView

--- src/graphics/views/AnimalView.java
+++ src/graphics/views/AnimalView.java
@@ -1,8 +1,15 @@
 package graphics.views;
 
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import animal.Animal;
+import def.Coord;
+import def.Game;
 import graphics.Drawable;
 
 public class AnimalView extends Drawable {
@@ -10,13 +17,28 @@ public class AnimalView extends Drawable {
 	private Animal a;
 	
 	public AnimalView(Animal a) {
+		
+		BufferedImage image;
+		try {
+			image = ImageIO.read(new File("resources/panda.png"));
+			this.SetImage(image);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		this.a = a;
 	}
 
 	@Override
-	public void Draw(Graphics g) {
-		// TODO Auto-generated method stub
-
+	public void Draw(Graphics g) {		
+		Coord actual = Game.GetCoords(a.getTile());
+		try {
+			g.drawImage(img, 0, 0, 50, 50, null);
+			
+		} catch (IOException e) {
+			System.out.println("Sikertelen állat kirajzolás.");
+		}
 	}
 
 }
