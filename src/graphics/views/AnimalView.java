f7446841246092f4c42766ab7966c4c1665e5fa3
Tuska <tuska.jozsef@outlook.com>
2019-05-12 23:37:38 +0200

    draw javítás

--- src/graphics/views/AnimalView.java
+++ src/graphics/views/AnimalView.java
@@ -1,7 +1,9 @@
 package graphics.views;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -36,7 +38,9 @@ public class AnimalView extends Drawable {
 			t = a.GetFollower().getTile();
 			Coord c2 = Game.MVPCoords(t);
 			g.setColor(Color.black);
-			g.drawLine((int)c2.GetX(), (int)c2.GetY(), (int)c.GetX(), (int)c.GetY());
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setStroke(new BasicStroke(6));
+			g2.drawLine((int)c2.GetX(), (int)c2.GetY(), (int)c.GetX(), (int)c.GetY());
 		}
 		
 		g.drawImage(img, (int)c.GetX() - 25, (int)c.GetY() - 25, 50, 50, null);
