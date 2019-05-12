cb06194fb591e68d311f01366f731b6ac8d191c0
Pogány Domonkos <pogi97@gmail.com>
2019-05-12 22:28:35 +0200

    asd

--- src/tile/Armchair.java
+++ src/tile/Armchair.java
@@ -58,30 +58,4 @@ public class Armchair extends Tile {
 	public void Accept(Animal a) {
 
 	}
-	
-	public void ListAttributes() {
-		String base = new String("NEVEM: %s\n" + 
-				"TÍPUSOM: Armchair\n" + 
-				"SZOMSZÉDAIM: ");
-		List<Object> obj = new ArrayList<Object>();
-		obj.add(this);
-		for(int i=0; i<neighbours.length; i++) {
-			obj.add(neighbours[i]);
-			if(i==neighbours.length-1)
-				base+= i+ " - %s\n";
-			else
-				base+= i+ " - %s, ";
-		}
-		
-		if(myAnimal==null)
-			base+= new String("A RAJTAM ÁLLÓ ÁLLAT: null\n");
-		else {
-			base+= new String("A RAJTAM ÁLLÓ ÁLLAT: %s\n");
-			obj.add(myAnimal);
-		}
-		
-		base+= new String("MÉG ENNYI KÖRIG PIHENSZ: " + count);
-		Object[] objArr=new Object[obj.size()];
-		objArr=obj.toArray();
-	}
 }
