9a6b4fd0e5bb93fa1a839f9ffb298e462b4c7bd8
Pogány Domonkos <pogi97@gmail.com>
2019-05-12 22:31:10 +0200

    nincs listattributes

--- src/animal/LazyPanda.java
+++ src/animal/LazyPanda.java
@@ -26,36 +26,4 @@ public class LazyPanda extends Panda {
 		this.DisableSteps();
 		System.out.println("beléptem");
 	}
-	
-	@Override
-	public void ListAttributes() {
-		String base = new String("NEVEM: %s\n" + "TÍPUSOM: LazyPanda\n" + "CSEMPE, AHOL ÁLLOK: [%s]\n");
-		List<Object> obj = new ArrayList<Object>();
-		obj.add(this);
-		obj.add(myTile);
-
-		if (follower == null)
-			base += new String("KÖVETÕM: null\n");
-		
-		else {
-			base += new String("KÖVETÕM: %s\n");
-			obj.add(follower);
-		}
-		
-		if(canStep)
-			base +=new String("LÉPHETEK: IGEN\n");
-		else
-			base +=new String("LÉPHETEK: NEM\n");
-		
-		if (leader == null)
-			base += new String("VEZETÕM: null");
-		else {
-			base += new String("VEZETÕM: %s");
-			obj.add(leader);
-		}
-
-		Object[] objArr = new Object[obj.size()];
-		objArr = obj.toArray();
-
-	}
 }
