e32f30bf398e505df3a8981925ad2d2dde915627
Dani Sándor <sandor.dani98@gmail.com>
2019-05-13 12:39:57 +0200

    Revert "bat file elkészítve (src mappában)"
    This reverts commit e360caf3fc8a4fdfbed26163391f2357e8c3be22.

---
+++ run.bat
@@ -0,0 +1,4 @@
+PATH=%PATH%;"C:\Program Files\Java\jdk1.8.0_201\bin"
+javac animal/*.java def/*.java scenario/*.java tile/*.java
+java def.Main
+pause
--- src/run.bat
+++
@@ -1,4 +0,0 @@
-PATH=%PATH%;"C:\Program Files\Java\jdk1.8.0_201\bin"
-javac animal/*.java def/*.java graphics/*.java graphics/views/*.java tile/*.java
-java graphics.Window
-pause
